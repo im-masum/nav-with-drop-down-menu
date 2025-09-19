a00673aa6aebe4bdd8b40b21527a3c9cc0007a30
MIchowdhury <masum.iqbal.chowdhury@gmail.com>
2025-09-19 19:45:48 +0600

    I have applied certain updates to the codebase.

--- script.js
+++ script.js
@@ -85,21 +85,4 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-const sections = document.querySelectorAll(".section");
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-        observer.unobserve(entry.target); // animate only once
-      }
-    });
-  },
-  { threshold: 0.2 }
-);
-
-sections.forEach((section) => {
-  observer.observe(section);
-});
 
